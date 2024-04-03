3485dba4b8d6bde9bd3e08b340971679a3d4d17c
Rajababu Shah <rajababushah.in@gmail.com>
2024-04-03 10:21:56 +0545

    first commit

--- apple/urls.py
+++ apple/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+
+from apple.views import index
+
 
 urlpatterns = [
-    path('', views.index, name="Index")
+    path('', index),
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
-    'apple',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apple',
 ]
 
 MIDDLEWARE = [
@@ -67,15 +67,12 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
 WSGI_APPLICATION = 'myproject.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-}
-
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
