1b42fe6d3b994c620631c0ee54922db9951ead17
Rajababu Shah <rajababushah.in@gmail.com>
2024-04-03 10:12:47 +0545

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
-
+WSGI_APPLICATION = 'myproject.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
